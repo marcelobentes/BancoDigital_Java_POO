07d4840ae5068edde68fd0b9ab3f43c9c9b1a250
Marcelo Bentes <marcelo.maia.m@gmail.com>
2023-02-13 23:02:55 -0300

    adicionando as classes

---
+++ src/Banco.java
@@ -0,0 +1,25 @@
+
+public class Banco {
+	private String nome;
+	private String cnpj;
+	private int agencia;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+}
---
+++ src/Conta.java
@@ -0,0 +1,32 @@
+
+public abstract class Conta {
+	protected int numero;
+	protected double saldo;
+	protected Cliente cliente;
+	protected Banco banco;
+	public void depositar () {
+		
+	}
+	public void transferir () {
+		
+	}
+	public void sacar () {
+		
+	}
+	public void saldo () {
+		
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+}
